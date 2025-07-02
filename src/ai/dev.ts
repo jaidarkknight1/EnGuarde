5a8de6ccf144f7732a833277178cbccbbcf54c67
jaidarkknight1 <74263535+jaidarkknight1@users.noreply.github.com>
2025-07-01 21:06:01 -0700

    Delete src directory

--- src/ai/dev.ts
+++
@@ -1,6 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/code-analysis.ts';
-import '@/ai/flows/code-recommendation-feedback.ts';
-import '@/ai/flows/improve-code.ts';
--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { CodeAnalyzer } from "@/components/code-analyzer";
-
-export default function HomePage() {
-  return (
-    <div className="container mx-auto py-2">
-      <CodeAnalyzer />
-    </div>
-  );
-}
